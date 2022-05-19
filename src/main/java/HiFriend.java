342073bab5c2fb832245ff60ddb5378d477f762c
9CD1KC2\admin <smithjrs22@gmail.com>
2022-05-19 13:15:06 -0400

    HiFriend test Green

---
+++ src/main/java/HiFriend.java
@@ -0,0 +1,6 @@
+public class HiFriend {
+    public static void main(String[] args) throws Exception {
+        String x= "get paid";
+        System.out.println("I love to " + x);
+    }
+}
---
+++ src/test/java/HiFriendTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HiFriendTest {
+
+    @Test
+    void printThisParticularString() {
+        assertEquals("I love to get paid", "I love to get paid");
+    }
+}
